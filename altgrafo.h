bdd588897e34221b5bb4a7c27e6efdc596f7ae5c
Mateus-SH <68969757+Mateus-SH@users.noreply.github.com>
2020-11-17 22:37:44 -0300

    Atualização do arquivo de cabeçalho do grafo
    *Adição dos cabeçalhos das funções que auxiliam a adição e remoção de vértices e arestas
    *Cabeçalho da função de representação

--- altgrafo.h
+++ altgrafo.h
@@ -25,7 +25,15 @@ struct Grafo *criarGrafo();
 struct Vertice *inserirVertice(struct Vertice *listaVertice,int rotulo);
 struct Vertice *removerVertice(struct Vertice *listaVertice, int rotulo);
 void listarVertice(struct Vertice *lista);
-
+struct Vertice *existeVertice(struct Vertice *lista , int rotulo);
+struct vAdjacente *existeAresta(struct Vertice *listaVertice, int origem ,int destino , int peso);
+struct Vertice *inserirAresta(struct Vertice *listaVertice, int origem, int destino, int peso);
+struct vAdjacente *insercaoAresta(struct vAdjacente *listaAdj, struct vAdjacente *novo);
+struct Vertice *removerAresta(struct Vertice *listaVertice , int origem , int destino ,int peso);
+struct vAdjacente *remocaoAresta(struct vAdjacente *vAdj, int rotuloVAdj, int peso);
+void listaDeAdjacencia(struct Vertice *listaVertice);
+struct vAdjacente *destruirVAdjacentes(struct vAdjacente *listaAdj);
+struct Vertice *removerVAdjComRotulo(struct Vertice *lista, int rotuloVAdj);
 
 
 #endif
